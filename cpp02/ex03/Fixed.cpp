f4b0cccf8efaf8a46c950f9ecbf57ece4c6e8090
mring <mring@student.42heilbronn.de>
2026-02-25 16:12:36 +0100

    cpp02 ex03 finished

---
+++ cpp02/ex03/Fixed.cpp
@@ -0,0 +1,172 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	value = 0;
+}
+
+// deconstructor
+Fixed::~Fixed()
+{
+}
+
+// copy constructor
+Fixed::Fixed(const Fixed &other)
+{
+	value = other.getRawBits();
+}
+
+// int constructor
+Fixed::Fixed(const int number)
+{
+	value = number << fractBits; // Shift left by 8 bits (multiply by 256)
+}
+
+// float constructor
+Fixed::Fixed(const float number)
+{
+	value = roundf(number * (1 << fractBits)); // Multiply by 256 and round
+}
+
+// arithmetic operators
+Fixed &Fixed::operator=(const Fixed &other)
+{
+	this->value = other.value;
+	return *this;
+}
+
+Fixed Fixed::operator+(const Fixed &other) const
+{
+	Fixed result;
+	result.value = this->value + other.value;
+	return result;
+}
+
+Fixed Fixed::operator-(const Fixed &other) const
+{
+	Fixed result;
+	result.value = this->value - other.value;
+	return result;
+}
+
+Fixed Fixed::operator*(const Fixed &other) const
+{
+	Fixed result;
+	result.value = (this->value * other.value) >> fractBits;
+	return result;
+}
+
+Fixed Fixed::operator/(const Fixed &other) const
+{
+	Fixed result;
+	result.value = (this->value << fractBits) / other.value;
+	return result;
+}
+
+// increment/decrement operators
+Fixed &Fixed::operator++()
+{
+	this->value++;
+	return *this;
+}
+
+Fixed Fixed::operator++(int)
+{
+	Fixed temp(*this);
+	this->value++;
+	return temp;
+}
+
+Fixed &Fixed::operator--()
+{
+	this->value--;
+	return *this;
+}
+
+Fixed Fixed::operator--(int)
+{
+	Fixed temp(*this);
+	this->value--;
+	return temp;
+}
+
+// comparison operators
+bool Fixed::operator>(const Fixed &other) const
+{
+	return this->value > other.value;
+}
+
+bool Fixed::operator<(const Fixed &other) const
+{
+	return this->value < other.value;
+}
+
+bool Fixed::operator>=(const Fixed &other) const
+{
+	return this->value >= other.value;
+}
+
+bool Fixed::operator<=(const Fixed &other) const
+{
+	return this->value <= other.value;
+}
+
+bool Fixed::operator==(const Fixed &other) const
+{
+	return this->value == other.value;
+}
+
+bool Fixed::operator!=(const Fixed &other) const
+{
+	return this->value != other.value;
+}
+
+// min/max
+Fixed &Fixed::min(Fixed &a, Fixed &b)
+{
+	return (a < b) ? a : b;
+}
+
+const Fixed &Fixed::min(const Fixed &a, const Fixed &b)
+{
+	return (a < b) ? a : b;
+}
+
+Fixed &Fixed::max(Fixed &a, Fixed &b)
+{
+	return (a > b) ? a : b;
+}
+
+const Fixed &Fixed::max(const Fixed &a, const Fixed &b)
+{
+	return (a > b) ? a : b;
+}
+
+// functions
+int Fixed::getRawBits(void) const
+{
+	return value;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	value = raw;
+}
+
+float Fixed::toFloat(void) const
+{
+	return (float)value / (1 << fractBits); // Divide by 256
+}
+
+int Fixed::toInt(void) const
+{
+	return value >> fractBits; // Shift right by 8 bits (divide by 256)
+}
+
+
+
+std::ostream &operator<<(std::ostream &os, const Fixed &fixed)
+{
+	os << fixed.toFloat(); // Print the float representation
+	return os;			   // Return stream for chaining
+}
---
+++ cpp02/ex03/Point.hpp
@@ -0,0 +1,19 @@
+#include "Fixed.hpp"
+
+class Point
+{
+private:
+	Fixed const x;
+	Fixed const y;
+public:
+	Point();
+	Point(const float x, const float y);
+	Point (const Point& other);
+	Point& operator=(const Point& other);
+	~Point();
+
+	Fixed getX() const;
+	Fixed getY() const;
+};
+
+bool bsp(Point const a, Point const b, Point const c, Point const point);
